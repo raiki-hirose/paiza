a7d441681dc88febcbf3c107ed77fc76c10010bf
raikihirose <raiki.hirose@asia-quest.jp>
2022-08-02 12:47:55 +0900

    B090

--- B090.php
+++ B090.php
@@ -1,1 +1,59 @@
 <?php
+
+function getStdin(): string
+{
+    return trim(fgets(STDIN));
+}
+
+function findMaxDevidedVoteParty(array $partyVotes): int
+{
+    global $devidedPartyVotes;
+    $maxDevidedVote = 0;
+    $maxDevidedVoteParty = -1;
+    $maxPartyVote = 0;
+    $partyCount = 0;
+    foreach ($devidedPartyVotes as $party) {
+        $max = $party[0];
+        if ($max > $maxDevidedVote) {
+            $maxDevidedVote = $max;
+            $maxDevidedVoteParty = $partyCount;
+            $maxPartyVote = $partyVotes[$partyCount];
+        } elseif($max === $maxDevidedVote && $partyVotes[$partyCount] > $maxPartyVote) {
+            $maxDevidedVote = $max;
+            $maxDevidedVoteParty = $partyCount;
+            $maxPartyVote = $partyVotes[$partyCount];
+        }
+        $partyCount++;
+    }
+    array_splice($devidedPartyVotes[$maxDevidedVoteParty], 0, 1);
+
+    return $maxDevidedVoteParty;
+}
+
+[$partyNum, $seatNum] = explode(' ', getStdin());
+$partyVotes = [];
+$devidedPartyVotes = [[]];
+$partyGetSeats = [];
+for ($i = 0; $i < $partyNum; $i++) {
+    $partyGetSeats[] = 0;
+}
+
+for ($i = 0; $i < $partyNum; $i++) {
+    $partyVotes[] = getStdin();
+}
+
+$partyCount = 0;
+foreach ($partyVotes as $partyVote) {
+    for ($i = 1; $i <= $seatNum; $i++) {
+        $devidedPartyVotes[$partyCount][] = (int)($partyVote / $i);
+    }
+    $partyCount++;
+}
+
+for ($i = 0; $i < $seatNum; $i++) {
+    $partyGetSeats[findMaxDevidedVoteParty($partyVotes)]++;
+}
+
+foreach ($partyGetSeats as $partySeat) {
+    echo $partySeat, PHP_EOL;
+}
